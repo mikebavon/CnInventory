97068798e184675645625ca5d346fc8824c5b198
mikebavon okello <bavomike@gmail.com>
2021-09-07 13:15:02 +0300

    -update

--- src/main/java/com/cohort/action/ItemCategoryAction.java
+++ src/main/java/com/cohort/action/ItemCategoryAction.java
@@ -1,8 +1,12 @@
 package com.cohort.action;
 
+import com.cohort.model.ItemCategory;
+
 import javax.servlet.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemCategoryAction implements Servlet {
     public void init(ServletConfig config) throws ServletException {
@@ -15,10 +19,36 @@ public class ItemCategoryAction implements Servlet {
 
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
 
+        List<ItemCategory> categories = new ArrayList<ItemCategory>();
+        categories.add(new ItemCategory("Food"));
+        categories.add(new ItemCategory("Furniture"));
+        categories.add(new ItemCategory("Utensils"));
+
         PrintWriter display = res.getWriter();
         display.print("<html>");
+        display.print("<head>"
+                + "<style>"
+                + "table, th, td {"
+                + "  border: 1px solid black;"
+                + "  width: 100%;"
+                + "  border-collapse: collapse;"
+                + "  background-color: #96D4D4;"
+                + "}"
+                + "</style>"
+                + "</head>");
         display.print("<body>");
-        display.print("This will handle inventory items category");
+
+        display.print("Item Categories<br/>");
+        display.print("<table>");
+        display.print("<th>Name</th>");
+
+        for (ItemCategory category : categories){
+            display.print("<tr>");
+            display.print("<td>" + category.getName() + "</td>");
+            display.print("</tr>");
+        }
+
+        display.print("</table>");
         display.print("</body>");
         display.print("</html>");
     }
---
+++ src/main/java/com/cohort/model/ItemCategory.java
@@ -0,0 +1,28 @@
+package com.cohort.model;
+
+public class ItemCategory {
+
+    public ItemCategory(String name){
+        this.name = name;
+    }
+
+    private int id;
+
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
